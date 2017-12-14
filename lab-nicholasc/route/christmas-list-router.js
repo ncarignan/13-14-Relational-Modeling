363f590b27b3905e813ace0aa07e5af20fdaa40d
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-12-13 19:48:02 -0800

    console log removed

--- lab-nicholasc/route/christmas-list-router.js
+++ lab-nicholasc/route/christmas-list-router.js
@@ -12,9 +12,7 @@ const christmasListRouter = module.exports = new Router();
 
 christmasListRouter.post('/api/christmas-lists', jsonParser, (request, response, next) => {
   logger.log('info', 'POST - processing request');
-  console.log(request.body);
-  // if(!request.body.name || !request.body.list || !request.body.pricelimit || !request.body.secretsanta) {
-  if(!request.body.name){
+  if(!request.body.name || !request.body.list || !request.body.pricelimit || !request.body.secretsanta) {
     return next(httpErrors(400, 'name, list, pricelimit, and secretsanta to be defined'));
   }
   return new ChristmasList(request.body).save()
