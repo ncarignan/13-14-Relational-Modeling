d504b8b4a71ad3935f79628140da61f052fe2e61
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-12-13 19:05:07 -0800

    test for christmas lists skeleton built

--- lab-nicholasc/__test__/server.test.js
+++ lab-nicholasc/__test__/server.test.js
@@ -8,7 +8,7 @@ const superagent = require('superagent');
 const Recipe = require('../model/recipe');
 const server = require('../lib/server');
 
-const apiURL = `http://localhost:${process.env.PORT}/api/recipes`;
+const apiURL = `http://localhost:${process.env.PORT}/api`;
 
 const recipeMockCreate = () => {
   return new Recipe({
@@ -28,8 +28,8 @@ const recipeMockCreateMany = (howMany) => {
 
 describe('/api/recipes', () => {
   beforeAll(server.start);
-  afterAll(server.stop);
   afterEach(() => Recipe.remove({})); //if we rmove after all, hopefullly sync wont break but if we do after each we have cleaner tests
+  afterAll(server.stop);
 
   describe('POST /api/recipes', () => {
     test('should respond with a recipe and 200 status code if there is no error',
@@ -38,7 +38,7 @@ describe('/api/recipes', () => {
           title : faker.lorem.words(10),
           content : faker.lorem.words(100),
         };
-        return superagent.post(`${apiURL}`)
+        return superagent.post(`${apiURL}/recipes`)
           .send(recipeToPost)
           .then(response => {
             expect(response.status).toEqual(200);
@@ -48,8 +48,8 @@ describe('/api/recipes', () => {
   describe('GET /api/recipes', () => {
     test('should return 10 notes where 10 is the size of the page by default if there is no error', () =>{
       return recipeMockCreateMany(100)
-        .then(tempRecipes =>{
-          return superagent.get(`${apiURL}`);
+        .then(() =>{
+          return superagent.get(`${apiURL}/recipes`);
         })
         .then(response => {
           expect(response.status).toEqual(200);
@@ -61,11 +61,9 @@ describe('/api/recipes', () => {
   });
   describe('GET /api/recipe:id', () => {
     test('should respond with recipes and 200 status code if there is no error', () =>{
-      let recipeToTest = null;
       recipeMockCreate()
         .then(recipe => {
-          recipeToTest = recipe;
-          return superagent.get(`${apiURL}/${recipe.id}`);
+          return superagent.get(`${apiURL}/recipes/${recipe.id}`);
         })
         .then(response => {
           expect(response.status).toEqual(200);
@@ -79,7 +77,7 @@ describe('/api/recipes', () => {
       return recipeMockCreate()
         .then(recipe => {
           recipeToUpdate = recipe;
-          return superagent.put(`${apiURL}/${recipe.id}`)
+          return superagent.put(`${apiURL}/recipes/${recipe.id}`)
             .send({title : 'grilled cheese'});
         })
         .then(response => {//only access to response but we want to test original
@@ -90,7 +88,7 @@ describe('/api/recipes', () => {
         });
     });
     test('should respond with a 404 error code if id invalid', () =>{
-      return superagent.delete(`${apiURL}/`)
+      return superagent.delete(`${apiURL}/recipes/`)
         .then(Promise.reject)
         .catch(response => {
           expect(response.status).toEqual(404);
@@ -101,14 +99,102 @@ describe('/api/recipes', () => {
     test('should respond with 204 status code if there is no error', () =>{
       return recipeMockCreate()
         .then(recipe => {
-          return superagent.delete(`${apiURL}/${recipe.id}`);
+          return superagent.delete(`${apiURL}/recipes/${recipe.id}`);
         })
         .then(response => {
           expect(response.status).toEqual(204);
         });
     });
     test('should respond with a 404 error code if id invalid', () =>{
-      return superagent.delete(`${apiURL}/`)
+      return superagent.delete(`${apiURL}/recipes/`)
+        .then(Promise.reject)
+        .catch(response => {
+          expect(response.status).toEqual(404);
+        });
+    });
+  });
+});
+describe('/api/christmas-list', () => {
+  beforeAll(server.start);
+  afterEach(() => Recipe.remove({})); //if we rmove after all, hopefullly sync wont break but if we do after each we have cleaner tests
+  afterAll(server.stop);
+
+  describe('POST /api/recipes', () => {
+    test('should respond with a recipe and 200 status code if there is no error',
+      () =>{
+        let recipeToPost = {
+          title : faker.lorem.words(10),
+          content : faker.lorem.words(100),
+        };
+        return superagent.post(`${apiURL}/christmas-lists`)
+          .send(recipeToPost)
+          .then(response => {
+            expect(response.status).toEqual(200);
+          });
+      });
+  });
+  describe('GET /api/christmas-lists', () => {
+    test('should return 10 notes where 10 is the size of the page by default if there is no error', () =>{
+      return recipeMockCreateMany(100)
+        .then(() =>{
+          return superagent.get(`${apiURL}/christmas-lists`);
+        })
+        .then(response => {
+          expect(response.status).toEqual(200);
+          expect(response.body.count).toEqual(100);
+          expect(response.body.data.length).toEqual(10);
+        });
+
+    });
+  });
+  describe('GET /api/recipe:id', () => {
+    test('should respond with recipes and 200 status code if there is no error', () =>{
+      recipeMockCreate()
+        .then(recipe => {
+          return superagent.get(`${apiURL}/christmas-lists/${recipe.id}`);
+        })
+        .then(response => {
+          expect(response.status).toEqual(200);
+        });
+    });
+  });
+  describe('PUT /api/christmas-lists:id', () => {
+    test('should update and respond with 200 status code if there is no error', () =>{
+      let recipeToUpdate = null;
+
+      return recipeMockCreate()
+        .then(recipe => {
+          recipeToUpdate = recipe;
+          return superagent.put(`${apiURL}/christmas-lists/${recipe.id}`)
+            .send({title : 'grilled cheese'});
+        })
+        .then(response => {//only access to response but we want to test original
+          expect(response.status).toEqual(200);
+          expect(response.body.title).toEqual('grilled cheese');
+          expect(response.body.content).toEqual(recipeToUpdate.content);
+          expect(response.body._id).toEqual(recipeToUpdate.id.toString());
+        });
+    });
+    test('should respond with a 404 error code if id invalid', () =>{
+      return superagent.delete(`${apiURL}/christmas-lists/`)
+        .then(Promise.reject)
+        .catch(response => {
+          expect(response.status).toEqual(404);
+        });
+    });
+  });
+  describe('DELETE /api/christmas-lists:id', () => {
+    test('should respond with 204 status code if there is no error', () =>{
+      return recipeMockCreate()
+        .then(recipe => {
+          return superagent.delete(`${apiURL}/christmas-lists/${recipe.id}`);
+        })
+        .then(response => {
+          expect(response.status).toEqual(204);
+        });
+    });
+    test('should respond with a 404 error code if id invalid', () =>{
+      return superagent.delete(`${apiURL}/christmas-lists/`)
         .then(Promise.reject)
         .catch(response => {
           expect(response.status).toEqual(404);
